1f987d4d9574877c7db7e9ad44ea811e613a3324
Inwebo <github@inwebo.net>
2025-07-19 18:19:17 +0200

    default routes

---
+++ config/routes.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+return function (RoutingConfigurator $routes): void {
+//    $routes
+//        ->add('app_index', '/')
+//        ->controller([IndexController::class, 'index'])
+//        ->defaults(['breadcrumb' => [
+//            'template' => 'Home page',
+//            'title' => 'Home',
+//            'parent' => null,
+//        ]]);
+};
